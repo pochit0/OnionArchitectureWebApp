2b15b40f33d38ca306b24b5b0da3b5ec4a209903
Luis Pool <pool.luis@gmail.com>
2022-01-31 18:15:50 -0600

    Generate the Migrations and the Database

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,9 +1,11 @@
+using Application;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Persistence;
 
 namespace WebApi
 {
@@ -21,17 +23,23 @@ namespace WebApi
         {
             services.AddControllers();
 
+            services.AddApplication();
+
+            services.AddPersistence(Configuration);
+
             #region Swagger
 
-            services.AddSwaggerGen(c => {
+            services.AddSwaggerGen(c =>
+            {
                 c.IncludeXmlComments(string.Format(@"{0}\OnionArchitecture.xml", System.AppDomain.CurrentDomain.BaseDirectory));
-                c.SwaggerDoc("v1", new OpenApiInfo {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
                     Version = "v1",
                     Title = "OnionArchitecture"
                 });
             });
 
-            #endregion
+            #endregion Swagger
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +48,7 @@ namespace WebApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }            
+            }
 
             app.UseHttpsRedirection();
 
@@ -59,11 +67,12 @@ namespace WebApi
             app.UseSwagger();
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
-            app.UseSwaggerUI(c => {
+            app.UseSwaggerUI(c =>
+            {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "OnionArchitecture");
             });
 
-            #endregion
+            #endregion Swagger
         }
     }
 }
